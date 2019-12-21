619883745ec660b63dfa88f999fa83a62fff1b0d
Nik <n.b.kobzev@gmail.com>
2019-12-21 13:27:52 +0300

    Поставил пробел

--- Домашнее задание урок 1/Task number 3.py
+++ Домашнее задание урок 1/Task number 3.py
@@ -1,4 +1,3 @@
-
 number_input = input('Введите число n: ')
 number_sum_int = int(number_input) + int(number_input + number_input) + int(number_input + number_input + number_input)
 print(f'Результат {number_input} + {number_input + number_input} + {number_input + number_input + number_input} = {number_sum_int}')
