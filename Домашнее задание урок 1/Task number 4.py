f34443723dbf0520f3a7f022b57c12e65efb45df
Nik <n.b.kobzev@gmail.com>
2019-12-21 12:21:27 +0300

    5 версия

---
+++ Домашнее задание урок 1/Task number 4.py
@@ -0,0 +1,10 @@
+number_input = input('Введите целое число n: ')
+i = 1
+number_max = number_input[0]
+
+while i < len(number_input):
+    if number_max < number_input[i]:
+        number_max = number_input[i]
+    i += 1
+
+print(f'Самая большая цифра в числе {number_max}')
