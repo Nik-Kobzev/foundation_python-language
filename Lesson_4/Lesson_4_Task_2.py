878c3de8d0ffde6a0e1b817ea8666431dcc02e0e
Nik <n.b.kobzev@gmail.com>
2020-01-20 21:26:13 +0300

    Удаление

--- Lesson_4/Lesson_4_Task_2.py
+++
@@ -1,5 +0,0 @@
-a = [4, 2, 3, 1, 5, 6, 1, 8]
-
-b = [value for index, value in enumerate(a) if index > 0 and a[index-1] < value]
-
-print(b)
--- Lesson_4/Lesson_4_Task_4.py
+++
@@ -1,6 +0,0 @@
-a = [3, 6, 12, 4, 23, 6, 4]
-
-b = [el for el in a if a.count(el) == 1]
-
-
-print(b)
